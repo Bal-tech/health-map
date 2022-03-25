12f65b36bd48e6975c81802daf21f9d99d9a1ebc
Bal-tech <77802610+Bal-tech@users.noreply.github.com>
2022-03-25 00:06:29 -0700

    added new file.

---
+++ java-collections-stack/src/com/collections/stack/Main.java
@@ -0,0 +1,9 @@
+package com.collections.stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("This is a new branch!");
+    }
+}
